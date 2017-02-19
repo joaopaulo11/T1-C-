33eb8d9ee558289e9cf72bef17ff9545a046f0f4
Joao Paulo <joao_paulorj010@hotmail.com>
2017-02-19 02:46:01 -0300

    Add project files.

---
+++ TP1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 45, 81, 29, 66, 03, 52, 51, 55, 74 };
+
+            Console.WriteLine("Numeros gerados:");
+
+            bubbleSort(numbers, numbers.Length);
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(numbers[i]);
+
+            Console.ReadLine();
+        }
+
+        static void bubbleSort(int[] arr, int length)
+        {
+            int repos = 0;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - (i + 1); j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        repos = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = repos;
+                    }
+                }
+            }
+        }
+    }
+}
